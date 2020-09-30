9cef7d1b3ba1ae11a03c03a0b04976a8c1203d9f
hermione <hermioneeth@gmail.com>
2020-09-30 21:04:43 +0700

    Add Gringotts balance schema and handle Transfer gETH event

--- generated/schema.ts
+++ generated/schema.ts
@@ -69,3 +69,150 @@ export class GETHTransfer extends Entity {
     this.set("value", Value.fromBigInt(value));
   }
 }
+
+export class Balance extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Balance entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Balance entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Balance", id.toString(), this);
+  }
+
+  static load(id: string): Balance | null {
+    return store.get("Balance", id) as Balance | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get amount(): BigInt {
+    let value = this.get("amount");
+    return value.toBigInt();
+  }
+
+  set amount(value: BigInt) {
+    this.set("amount", Value.fromBigInt(value));
+  }
+}
+
+export class GringottsSummary extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save GringottsSummary entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save GringottsSummary entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("GringottsSummary", id.toString(), this);
+  }
+
+  static load(id: string): GringottsSummary | null {
+    return store.get("GringottsSummary", id) as GringottsSummary | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get gETHSupply(): BigInt {
+    let value = this.get("gETHSupply");
+    return value.toBigInt();
+  }
+
+  set gETHSupply(value: BigInt) {
+    this.set("gETHSupply", Value.fromBigInt(value));
+  }
+
+  get totalETH(): BigInt {
+    let value = this.get("totalETH");
+    return value.toBigInt();
+  }
+
+  set totalETH(value: BigInt) {
+    this.set("totalETH", Value.fromBigInt(value));
+  }
+}
+
+export class Position extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Position entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Position entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Position", id.toString(), this);
+  }
+
+  static load(id: string): Position | null {
+    return store.get("Position", id) as Position | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get goblin(): Bytes {
+    let value = this.get("goblin");
+    return value.toBytes();
+  }
+
+  set goblin(value: Bytes) {
+    this.set("goblin", Value.fromBytes(value));
+  }
+
+  get owner(): Bytes {
+    let value = this.get("owner");
+    return value.toBytes();
+  }
+
+  set owner(value: Bytes) {
+    this.set("owner", Value.fromBytes(value));
+  }
+
+  get debtShare(): BigInt {
+    let value = this.get("debtShare");
+    return value.toBigInt();
+  }
+
+  set debtShare(value: BigInt) {
+    this.set("debtShare", Value.fromBigInt(value));
+  }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
   Gringotts,
   AddDebt,
@@ -9,7 +9,7 @@ import {
   RemoveDebt,
   Transfer
 } from "../generated/Gringotts/Gringotts"
-import { GETHTransfer } from "../generated/schema"
+import { GETHTransfer, Balance, GringottsSummary, Position } from "../generated/schema"
 
 /* export function handleAddDebt(event: AddDebt): void {
   // Entities can be loaded from the store using a string ID; this ID
@@ -70,7 +70,14 @@ import { GETHTransfer } from "../generated/schema"
   // - contract.totalETH(...)
 } */
 
-export function handleAlohomora(event: Alohomora): void { }
+export function handleAddDebt(event: AddDebt) : void {}
+
+export function handleAlohomora(event: Alohomora): void { 
+  // const id = event.params.id.toString()
+  // const position = new Position(id)
+
+
+}
 
 export function handleApproval(event: Approval): void { }
 
@@ -81,11 +88,38 @@ export function handleOwnershipTransferred(event: OwnershipTransferred): void { 
 export function handleRemoveDebt(event: RemoveDebt): void { }
 
 export function handleTransfer(event: Transfer): void {
-  let transactionHash = event.transaction.hash.toHexString()
-  let transfer = new GETHTransfer(transactionHash)
+  let transactionId = event.transaction.hash.toHexString() + "-" + event.logIndex.toHexString()
+  let transfer = new GETHTransfer(transactionId)
 
   transfer.from = event.params.from
   transfer.to = event.params.to
   transfer.value = event.params.value
   transfer.save()
+
+  // update sender account balance
+  let sender = Balance.load(event.params.from.toHexString())
+  if (sender == null) {
+    sender = new Balance(event.params.from.toHexString())
+    sender.amount = BigInt.fromI32(0)
+  }
+  sender.amount = sender.amount.minus(transfer.value)
+  sender.save()
+
+  // update recipient account balance
+  let recipient = Balance.load(event.params.to.toHexString())
+  if (recipient == null) {
+    recipient = new Balance(event.params.to.toHexString())
+    recipient.amount = BigInt.fromI32(0)
+  }
+  recipient.amount = recipient.amount.plus(transfer.value)
+  recipient.save()
+
+  let summary = GringottsSummary.load("Gringotts")
+  if (summary == null) {
+    summary = new GringottsSummary("Gringotts")
+  }
+  let gringotts = Gringotts.bind(event.address);
+  summary.gETHSupply = gringotts.totalSupply()
+  summary.totalETH = gringotts.totalETH()
+  summary.save()
 }
